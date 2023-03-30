b47cdb7f3f54095dd3fa532d1fdc8dc1ac215e56
Prashant Negi <prashantnegi878@gmail.com>
2023-03-30 12:16:49 +0530

    Time: 102 ms (23.25%), Space: 43.4 MB (28.38%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    Map<String, Boolean> m = new HashMap<>();
+
+    public boolean isScramble(String s1, String s2) {
+        int n = s1.length();
+        if (s1.equals(s2)) return true;
+        if (n == 1) return false;
+
+        String key = s1 + " " + s2;
+
+        if (m.containsKey(key)) return m.get(key);
+
+        for (int i = 1; i < n; i++) {
+            if (isScramble(s1.substring(0, i), s2.substring(0, i)) && 
+                isScramble(s1.substring(i), s2.substring(i))){
+                m.put(key, true);
+                return true;
+            }
+
+            if (isScramble(s1.substring(0, i), s2.substring(n - i)) && 
+                isScramble(s1.substring(i), s2.substring(0, n - i))){
+                m.put(key, true);
+                return true;
+            }
+        }
+
+        m.put(key, false);
+        return false;
+    }
+}
