942c93c41a45730b81f55e92a73bd8c757c909d4
Prashant Negi <prashantnegi878@gmail.com>
2023-12-20 20:48:56 +0530

    Time: 14 ms (5.97%) | Memory: 44.9 MB (5.53%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int num1=prices[0],num2=prices[1];
+        for(int i=1;i<prices.length;i++)
+        {
+            if(prices[i]<num1)
+            {
+                num2=num1;
+                num1=prices[i];
+            }
+            else if(prices[i]<num2)
+            num2=prices[i];
+        }
+        System.out.println(num1 +" - "+num2);
+        int sum=num1+num2;
+        return (money-sum>=0)? money-sum : money;
+    }
+}
