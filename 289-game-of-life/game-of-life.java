748a073de58dc51438ece6a88c09338c574e658c
Prashant Negi <prashantnegi878@gmail.com>
2023-12-25 03:13:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (5.26%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,19 +1,17 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-        int[][] result = new int[board.length][board[0].length];
         for(int i=0;i<board.length;i++)
         {
             for(int j=0;j<board[0].length;j++)
             {
-                result[i][j]=calulateNeighbours(board,i,j);
+                board[i][j]=calulateNeighbours(board,i,j);
             }
         }
-        // return result;
         for(int i=0;i<board.length;i++)
         {
-            for(int j=0;j<board[i].length;j++)
+            for(int j=0;j<board[0].length;j++)
             {
-                board[i][j]=result[i][j];
+                board[i][j]=(board[i][j]==2)?1:(board[i][j]==3)?0:board[i][j];
             }
         }
     }
@@ -29,12 +27,17 @@ class Solution {
             for(int j=-1;j<=1;j++)
             {
                 if(y+j<0 || y+j>=l2) continue;
-                total+=board[x+i][y+j];
+                total+=(board[x+i][y+j]==2)?0:(board[x+i][y+j]==3)?1:board[x+i][y+j];
             }
         }
         total-=board[x][y];
-        if(total==2 && board[x][y]==1)  return 1;
-        else if(total==3) return 1;
-        else return 0;
+        int result;
+        if(total==2 && (board[x][y]==1 || board[x][y]==3))  result=1;
+        else if(total==3) result=1;
+        else result= 0;
+
+        if((board[x][y]==0 || board[x][y]==2)&& result==1)return 2;
+        else if((board[x][y]==1 || board[x][y]==3)&& result==0) return 3;
+        else return board[x][y];
     }
 }
