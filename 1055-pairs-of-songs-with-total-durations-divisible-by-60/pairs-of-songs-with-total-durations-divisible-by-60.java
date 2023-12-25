ef48933766c69dc7123cd371b32466f3fb2e3411
Prashant Negi <prashantnegi878@gmail.com>
2023-12-26 02:35:14 +0530

    Time: 4 ms (49.20%) | Memory: 53.3 MB (29.88%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -5,16 +5,11 @@ class Solution {
         int arr[] = new int[60];
         for(int i=0;i<l;i++){
             time[i]%=60;
-            arr[time[i]] += 1;
-        }
-        arr[30] -=1;
-        arr[0] -=1;
-        for(int i=0;i<l;i++){
-            if(arr[ (60 - time[i]) % 60] != 0)
-            {
+            if(arr[ (60 - time[i]) % 60] != 0){
                 total+= arr[(60 - time[i]) % 60];
-                arr[time[i]] -=1;
-            } 
+            }
+            arr[time[i]] += 1; 
+            
         }
         return total;
     }
