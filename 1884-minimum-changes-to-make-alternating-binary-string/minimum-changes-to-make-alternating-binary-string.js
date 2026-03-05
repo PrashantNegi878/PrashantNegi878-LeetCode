2e5b161764b363ce1cc0684eeb38580bf1330e7f
Prashant Negi <prashantnegi878@gmail.com>
2026-03-06 01:12:35 +0530

    Time: 6 ms (10.87%) | Memory: 55 MB (30.44%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minOperations = function(s) {
+    let result1=0, result2 = 0;
+    let alter=true;
+    let arr=s.split("");
+    let arr2=s.split("");
+    for(let i=0;i<arr.length;i++){
+        // console.log(Number(alter))
+        if(arr[i] == alter){
+            result1++;
+            arr[i]=Number(!alter);
+            // console.log("Arr")
+        } 
+
+        if(arr2[i] == (!alter)){
+            result2++;
+            arr[i]=Number(alter);
+        } 
+        alter=!alter;
+    }
+    return Math.min(result1,result2);
+};
